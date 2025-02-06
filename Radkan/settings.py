e7ccfb0be1d5175a4708c52c4413fd5721925a9d
Masoud <masoudnk2@gmail.com>
2025-02-06 20:13:45 +0330

    starting

--- Radkan/settings.py
+++ Radkan/settings.py
@@ -149,13 +149,23 @@ WSGI_APPLICATION = 'Radkan.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         'NAME': 'radkan',  # database name
+#         'USER': 'postgres',
+#         'PASSWORD': 'mn1860107',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         'NAME': 'radkan',  # database name
-        'USER': 'postgres',
-        'PASSWORD': 'mn1860107',
-        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'g7lUe9jrnUSrj8hrouik7BAw',
+        'HOST': 'radkan-attendance-db',
         'PORT': '5432',
     }
 }
